48d3ebb13c857ebb8a046bbc7c8f9c12a6aca60d
raj <ranajiagd6788@gmail.com>
2021-09-05 08:50:50 +0530

    all about file

---
+++ loggin.py
@@ -0,0 +1,8 @@
+f=open('file1.py','r')
+print('file name',f.name)
+print('file mode',f.mode)
+print("is file readable",f.readable())
+print("is file writable",f.writable())
+print("is file closed",f.closed)
+f.close()
+print("is file closed",f.closed)
